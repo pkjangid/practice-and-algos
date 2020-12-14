506bb61ac99d1eb25517170ae6445b33cb91d7af
Sachin <sachinjangid021@gmail.com>
2020-12-14 21:20:34 +0530

    pyramid program

---
+++ pyramid.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+   int rows, coef = 1, space, i, j;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 0; i < rows; i++) {
+      for (space = 1; space <= rows - i; space++)
+         printf(" ");
+      for (j = 0; j <= i; j++) {
+         
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
