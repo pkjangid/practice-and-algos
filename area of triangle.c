54def838361c709708f33942b4fcb1f0651e3f62
pkjangid <75982077+pkjangid@users.noreply.github.com>
2020-12-25 18:33:28 +0530

    find the area of triangle
    how to find area of triangle in c language ?
    check this file

---
+++ area of triangle.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("\n\n\t\t*सपनों के चक्कर में जीना भूल जाना अच्छा नहीं हैं।*\n\n\n");
+    int h, b;
+    float area;
+    printf("\n\nEnter the height of the Triangle: ");
+    scanf("%d", &h);
+    printf("\n\nEnter the base of the Triangle: ");
+    scanf("%d", &b);
+
+    /*
+        Formula for the area of the triangle = (height x base)/2
+        
+        Also, typecasting denominator from int to float 
+        to get the output in float
+    */
+    area = (h*b)/(float)2;
+    printf("\n\n\nThe area of the triangle is: %f", area);
+
+    printf("\n\n\t\t\tmade by me !\n\n\n");
+    return 0;
+}
