d4bc91059017fd4a62891690b7b279f27fc35117
pkjangid <75982077+pkjangid@users.noreply.github.com>
2020-12-27 16:27:18 +0530

    how to calculate GST
    in this file i am showing how to calculate GST in total price
    the GST formula is
    GST = (total price/100)*%GST
    
    is a very simple code . try this

---
+++ gst.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{ int g, s ;
+   float gst;
+   float total;
+ printf("  \n\n\n            *** this code is my invation not copy paste ***     \n\n");
+ printf("\n\nenter the value to find GST\n");
+ scanf("%d",& g);
+ printf("How many % want to GST\n");
+ scanf("%d",& s);
+ gst = (g/100)*s;
+ total = gst+g;
+ printf("your total price is =%f",total);
+ printf("\n\nthank you very much\n\n\n");
+ printf("==================== END =================");
+}
