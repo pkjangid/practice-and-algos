aad8a886afb824a1f77d9d883c02e35d1544e485
Pritam Jangid <75982077+pkjangid@users.noreply.github.com>
2021-04-15 20:24:40 +0530

    Create bubble_sort.c

---
+++ bubble_sort.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void bubble_sort(int A[], int n)
+{
+  int round,i,temp;
+   for(round=0;round<=n-1;round++)
+      for(i=0;i<=n-1-round;i++)
+         if(A[i]>A[i+1])
+           {
+             temp=A[i];
+             A[i]=A[i+1];
+             A[i+1]=temp;
+            }
+}
+int main()
+{
+  int A[]={12.45.78.23.56.89.13.46.79}
+  int i
+  bubble_sort(A,9)
+  for(i=0;i<8;i++)
+     printf("%d\n",A[i]);
+  return 0;
+}
