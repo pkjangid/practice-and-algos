bd35a3d27d80df84946c6871b38497c00fb8e41b
Pritam Jangid <75982077+pkjangid@users.noreply.github.com>
2021-09-18 21:19:34 +0530

    Update pyramid.c

--- pyramid.c
+++ pyramid.c
@@ -6,7 +6,8 @@ int main() {
    for (i = 0; i < rows; i++) {
       for (space = 1; space <= rows - i; space++)
          printf(" ");
-      for (j = 0; j <= i; j++) {
+      
+for (j = 0; j <= i; j++) {
          
         
  printf("* ");
